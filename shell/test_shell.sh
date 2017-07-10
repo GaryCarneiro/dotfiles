7ea3ee0f624af7d3f80c2926a6373e28b32af6bb
Garfield Carneiro <garfield@bluejeans.com>
2017-07-10 17:02:22 +0530

    Script to take backup of existing data

---
+++ shell/test_shell.sh
@@ -0,0 +1,19 @@
+
+datefmt=`date "+%d%b%Y-%H%M%S"`
+# Backup Directory
+bdir="/tmp/zsh_backup_$datefmt"
+echo "Backup Directory is $bdir"
+mkdir -v $bdir
+
+
+cp -v ~/.zshrc  $bdir
+cp -v ./zshrc ~/.zshrc
+
+cp -v ~/.zsh_custom_profile  $bdir
+cp -v ./zsh_custom_profile ~/.zsh_custom_profile
+
+cp -v ~/.aliases  $bdir
+cp -v ./aliases ~/.aliases
+
+cp -v ~/.functions  $bdir
+cp -v ./functions ~/.functions
