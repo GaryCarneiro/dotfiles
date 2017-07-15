9e78a3a2c95b63c419a59a4f63c74cae82861a1e
Garfield Carneiro <garfield@bluejeans.com>
2017-07-15 22:12:50 +0530

    Directory test not file

--- shell/anapy/anapy.plugin.zsh
+++ shell/anapy/anapy.plugin.zsh
@@ -1,12 +1,12 @@
-if [ -f ~/.anaconda/bin ] ; then
+if [ -d $HOME/.anaconda/bin ] ; then
   path=("$HOME/.anaconda/bin" $path)
 fi
 
-if [ -f ~/.anaconda/2/bin ] ; then
+if [ -d $HOME/.anaconda/2/bin ] ; then
   path=("$HOME/.anaconda/2/bin" $path)
 fi
 
-if [ -f ~/.anaconda/3/bin ] ; then
+if [ -d $HOME/.anaconda/3/bin ] ; then
   path=("$HOME/.anaconda/3/bin" $path)
 fi
 
