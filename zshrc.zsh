7df90840c7bd74c7314814c2060f4869e35be023
Garfield Carneiro <garfield@bluejeans.com>
2018-05-13 13:14:24 +0530

    Moving nonZSH to custom_profile

--- zshrc.zsh
+++ zshrc.zsh
@@ -1,31 +1,5 @@
 #!/usr/bin/env zsh
 
-# VIM CONFIG
-if ! [[ -f ~/.vimrc ]] ; then
-  echo "Downloading Vimrc."
-  curl -s -L 'https://git.io/vpDo4' -o ~/.vimrc
-  echo "Cloning Vundle."
-  git clone https://github.com/VundleVim/Vundle.vim.git \
-  ~/.vim/bundle/Vundle.vim
-fi
-# TMUX CONFIG
-if ! [[ -f ~/.tmux.conf ]] ; then
-  echo "Downloading tmux.conf"
-  curl -s -L 'https://git.io/vpDoX' -o ~/.tmux.conf
-fi
-
-# Ruby GEMRC
-if ! [[ -f ~/.gemrc ]] ; then
-  echo "Downloading GemRC"
-  curl -s -L 'https://git.io/vpShg' -o ~/.gemrc
-fi
-
-# Ruby IRBRC
-if ! [[ -f ~/.irbrc ]] ; then
-  echo "Downloading IRBRC."
-  curl	 -s -L 'https://git.io/vpShK' -o ~/.irbrc
-fi
-
 # ANTIGEN
 if [[ -f ~/.antigen.zsh ]] ; then
   source ~/.antigen.zsh
