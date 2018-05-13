32496a9792a1775b1e6efe402b2b9a0bf6aadaf6
Garfield Carneiro <garfield@bluejeans.com>
2018-05-13 13:07:49 +0530

    2 space ident

--- functions.zsh
+++ functions.zsh
@@ -2,8 +2,8 @@
 # This function cert fetches SSL Certificate on a specified port or defaults the port to 443
 
 function cert() {
-    ( [ -z $2 ] ) &&  export port=443  ||  export port=$2  
-    openssl s_client -connect $1:$port
+  ( [ -z $2 ] ) &&  export port=443  ||  export port=$2  
+  openssl s_client -connect $1:$port
 }
 
 
@@ -20,20 +20,20 @@ alias tn='ta'
 
 # This functions zaps the host from your ssh KnownHost file
 function keydel() {
-     [ -z $1 ] && echo  "Please pass some hostname as argument" || ssh-keygen -R $1 
+  [ -z $1 ] && echo  "Please pass some hostname as argument" || ssh-keygen -R $1 
 }
 
 # http://boredzo.org/blog/archives/2016-08-15/colorized-man-pages-understood-and-customized
 man() {
-    env \
-        LESS_TERMCAP_mb=$(printf "\e[1;31m") \
-        LESS_TERMCAP_md=$(printf "\e[1;31m") \
-        LESS_TERMCAP_me=$(printf "\e[0m") \
-        LESS_TERMCAP_se=$(printf "\e[0m") \
-        LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
-        LESS_TERMCAP_ue=$(printf "\e[0m") \
-        LESS_TERMCAP_us=$(printf "\e[1;32m") \
-            man "$@"
+  env \
+    LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+    LESS_TERMCAP_md=$(printf "\e[1;31m") \
+    LESS_TERMCAP_me=$(printf "\e[0m") \
+    LESS_TERMCAP_se=$(printf "\e[0m") \
+    LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+    LESS_TERMCAP_ue=$(printf "\e[0m") \
+    LESS_TERMCAP_us=$(printf "\e[1;32m") \
+    man "$@"
 }
 
 # SSH via JumpBox
@@ -42,12 +42,11 @@ man() {
 jssh() {
 usage="Usage : $0 <ip-address>"
 u=$USER
-if [ $# -lt 1 ];
-    then
-    echo $usage
-    else
-        ssh -A -tt $u@jumpbox ssh -tt $u@$1
-    fi
+if [ $# -lt 1 ]; then
+  echo $usage
+else
+  ssh -A -tt $u@jumpbox ssh -tt $u@$1
+fi
 }
 
 jscp() {
@@ -56,25 +55,23 @@ jscp() {
  scp -oProxyJump=jumpbox $1 $2
 }
 
-
-
 dot_files_refresh () {
- TIMESTAMP=$(date "+%h%d-%Y-%H%M%S")
- BDIR="/tmp/garfield/dotfiles-$TIMESTAMP"
- echo "Refreshing Profile. Backing up in $BDIR"
- echo "Press Ctrl+C to exit or ENTER to continue"
- read
- mkdir -pv $BDIR/
- mv -v ~/.zshrc $BDIR/
- curl -s -L -o ~/.zshrc 'https://git.io/vpSjJ'
- mv -v ~/.vimrc $BDIR/
- rm -rf ~/.vim/bundle/Vundle.vim
- rm -rf ~/.antigen
- mv -v ~/.functions.zsh $BDIR/
- mv -v ~/.tmux.conf $BDIR/
- mv -v ~/.gemrc $BDIR
- mv -v ~/.irbrc $BDIR
- curl -s -L -o ~/.zshrc 'https://git.io/vpDFJ'
+  TIMESTAMP=$(date "+%h%d-%Y-%H%M%S")
+  BDIR="/tmp/garfield/dotfiles-$TIMESTAMP"
+  echo "Refreshing Profile. Backing up in $BDIR"
+  echo "Press Ctrl+C to exit or ENTER to continue"
+  read
+  mkdir -pv $BDIR/
+  /bin/mv -v ~/.zshrc $BDIR/
+  curl -s -L -o ~/.zshrc 'https://git.io/vpSjJ'
+  /bin/mv -v ~/.vimrc $BDIR/
+  rm -rf ~/.vim/bundle/Vundle.vim
+  rm -rf ~/.antigen
+  /bin/mv -v ~/.functions.zsh $BDIR/
+  /bin/mv -v ~/.tmux.conf $BDIR/
+  /bin/mv -v ~/.gemrc $BDIR
+  /bin/mv -v ~/.irbrc $BDIR
+  curl -s -L -o ~/.zshrc 'https://git.io/vpDFJ'
 }
 
 
